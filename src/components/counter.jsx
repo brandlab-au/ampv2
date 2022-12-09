a29acb2516bf742bea6e9c29e65412a6df8464e1
brandlab-au <jon@brandlab.co.uk>
2022-12-09 13:07:37 +0000

    using map in a function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,19 +1,29 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = { count: [0, 55, 66, 7] };
+  state = { count: [0, 55, 66, 7], url: "https://picsum.photos/300" };
+  style = { fontSize: 50 };
   render() {
     return (
-      <p>
-        <p>this is the top part</p>
+      <div>
+        <p>Coolie O</p>
         Nice to see you here {this.formater()}
-        {this.state.count.map((c) => (
-          <li>c</li>
-        ))}
-      </p>
+        {this.renderTag()}
+        <button className="badge badge-primary m-2 text-success">
+          Increament
+        </button>
+        <hr></hr>
+        <img src={this.state.url} />
+        <p style={this.style} class="text-success">
+          .Loving it
+        </p>
+      </div>
     );
   }
-
+  renderTag() {
+    if (this.state.count == 0) return "Nothing in tags";
+    return this.state.count.map((c) => <li key={c}>{c}</li>);
+  }
   formater() {
     const short = this.state.count;
     return short[0] === 0 ? "Zero" : short;
