b0dd85267328c9c4a7af3edd340c529921b423d6
brandlab-au <jon@brandlab.co.uk>
2022-12-09 12:19:13 +0000

    new component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = { count: 34 };
+  state = { count: [0, 55, 66, 7] };
   render() {
-    return <p>Nice to see yuou {this.state.count}</p>;
+    return (
+      <p>
+        <p>this is the top part</p>
+        Nice to see you here {this.formater()}
+        {this.state.count.map((c) => (
+          <li>c</li>
+        ))}
+      </p>
+    );
+  }
+
+  formater() {
+    const short = this.state.count;
+    return short[0] === 0 ? "Zero" : short;
   }
 }
 
